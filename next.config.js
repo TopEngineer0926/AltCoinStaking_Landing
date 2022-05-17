36ae8fe90a22a87b2db404b9c59024c2b4ea097c
TopEngineer0926 <top_engineer_0926@outlook.com>
2022-05-17 01:35:50 -0400

    updated the config file

--- next.config.js
+++ next.config.js
@@ -10,10 +10,3 @@ module.exports = withPlugins([
     // your other plugins here
   
   ]);
-
-module.exports = {
-  env: {
-    REACT_APP_NFT_ADDRESS: '0xc004b3091e0B92B99a41279ed16d0741Bfd108d8',
-    REACT_APP_CHAIN_ID: 80001
-  }
-}
